483821a5aff4a9fa27f0d7add4aa2d7960a62823
chshin10 <37797209+chshin10@users.noreply.github.com>
2018-03-29 00:30:11 +0900

    contrib

--- epinet_func/func_pfm.py
+++ epinet_func/func_pfm.py
@@ -13,6 +13,36 @@ loaded image and the scale factor from the file.
 import numpy as np
 import sys
 import re
+
+
+############################################################################
+#  This file is part of the 4D Light Field Benchmark.                      #
+#                                                                          #
+#  This work is licensed under the Creative Commons                        #
+#  Attribution-NonCommercial-ShareAlike 4.0 International License.         #
+#  To view a copy of this license,                                         #
+#  visit http://creativecommons.org/licenses/by-nc-sa/4.0/.                #
+#                                                                          #
+#  Authors: Katrin Honauer & Ole Johannsen                                 #
+#  Contact: contact@lightfield-analysis.net                                #
+#  Website: www.lightfield-analysis.net                                    #
+#                                                                          #
+#  The 4D Light Field Benchmark was jointly created by the University of   #
+#  Konstanz and the HCI at Heidelberg University. If you use any part of   #
+#  the benchmark, please cite our paper "A dataset and evaluation          #
+#  methodology for depth estimation on 4D light fields". Thanks!           #
+#                                                                          #
+#  @inproceedings{honauer2016benchmark,                                    #
+#    title={A dataset and evaluation methodology for depth estimation on   #
+#           4D light fields},                                              #
+#    author={Honauer, Katrin and Johannsen, Ole and Kondermann, Daniel     #
+#            and Goldluecke, Bastian},                                     #
+#    booktitle={Asian Conference on Computer Vision},                      #
+#    year={2016},                                                          #
+#    organization={Springer}                                               #
+#    }                                                                     #
+#                                                                          #
+############################################################################
     
 def write_pfm(data, fpath, scale=1, file_identifier=b'Pf', dtype="float32"):
     # PFM format definition: http://netpbm.sourceforge.net/doc/pfm.html
@@ -33,6 +63,7 @@ def write_pfm(data, fpath, scale=1, file_identifier=b'Pf', dtype="float32"):
 
         file.write(values)
         
+# Code from https://gist.github.com/chpatrick/8935738        
 def load_pfm(file):
   color = None
   width = None
