94e44dd2e3848ed5b7fc7b7cd10c13f5c5deb753
chshin10 <37797209+chshin10@users.noreply.github.com>
2018-04-09 13:00:31 +0900

    +load_pfm

--- epinet_fun/func_pfm.py
+++ epinet_fun/func_pfm.py
@@ -63,63 +63,50 @@ def write_pfm(data, fpath, scale=1, file_identifier=b'Pf', dtype="float32"):
 
         file.write(values)
         
-# Code from https://gist.github.com/chpatrick/8935738        
-def load_pfm(file):
-  color = None
-  width = None
-  height = None
-  scale = None
-  endian = None
+def _get_next_line(f):
+    next_line = f.readline().decode('utf-8').rstrip()
+    # ignore comments
+    while next_line.startswith('#'):
+        next_line = f.readline().rstrip()
+    return next_line
+        
+def read_pfm(fpath, expected_identifier="Pf"):
+    # PFM format definition: http://netpbm.sourceforge.net/doc/pfm.html
 
-  header = file.readline().rstrip()
-  if header == 'PF':
-    color = True    
-  elif header == 'Pf':
-    color = False
-  else:
-    raise Exception('Not a PFM file.')
+    with open(fpath, 'rb') as f:
+        #  header
+        identifier = _get_next_line(f)
+        if identifier != expected_identifier:
+            raise Exception('Unknown identifier. Expected: "%s", got: "%s".' % (expected_identifier, identifier))
 
-  dim_match = re.match(r'^(\d+)\s(\d+)\s$', file.readline())
-  if dim_match:
-    width, height = map(int, dim_match.groups())
-  else:
-    raise Exception('Malformed PFM header.')
+        try:
+            line_dimensions = _get_next_line(f)
+            dimensions = line_dimensions.split(' ')
+            width = int(dimensions[0].strip())
+            height = int(dimensions[1].strip())
+        except:
+            raise Exception('Could not parse dimensions: "%s". '
+                            'Expected "width height", e.g. "512 512".' % line_dimensions)
 
-  scale = float(file.readline().rstrip())
-  if scale < 0: # little-endian
-    endian = '<'
-    scale = -scale
-  else:
-    endian = '>' # big-endian
+        try:
+            line_scale = _get_next_line(f)
+            scale = float(line_scale)
+            assert scale != 0
+            if scale < 0:
+                endianness = "<"
+            else:
+                endianness = ">"
+        except:
+            raise Exception('Could not parse max value / endianess information: "%s". '
+                            'Should be a non-zero number.' % line_scale)
 
-  data = np.fromfile(file, endian + 'f')
-  shape = (height, width, 3) if color else (height, width)
-  return np.reshape(data, shape), scale
+        try:
+            data = np.fromfile(f, "%sf" % endianness)
+            data = np.reshape(data, (height, width))
+            data = np.flipud(data)
+            with np.errstate(invalid="ignore"):
+                data *= abs(scale)
+        except:
+            raise Exception('Invalid binary values. Could not create %dx%d array from input.' % (height, width))
 
-'''
-Save a Numpy array to a PFM file.
-'''
-def save_pfm(file, image, scale = 1):
-  color = None
-
-  if image.dtype.name != 'float32':
-    raise Exception('Image dtype must be float32.')
-
-  if len(image.shape) == 3 and image.shape[2] == 3: # color image
-    color = True
-  elif len(image.shape) == 2 or len(image.shape) == 3 and image.shape[2] == 1: # greyscale
-    color = False
-  else:
-    raise Exception('Image must have H x W x 3, H x W x 1 or H x W dimensions.')
-
-  file.write('PF\n' if color else 'Pf\n')
-  file.write('%d %d\n' % (image.shape[1], image.shape[0]))
-
-  endian = image.dtype.byteorder
-
-  if endian == '<' or endian == '=' and sys.byteorder == 'little':
-    scale = -scale
-
-  file.write('%f\n' % scale)
-
-  image.tofile(file)  
+        return data 
