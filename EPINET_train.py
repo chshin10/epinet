153c4621252b5e05f05fcd0f2d61d970a5b7add8
chshin10 <37797209+chshin10@users.noreply.github.com>
2018-11-29 11:38:11 +0900

    update typo
    gpu0

--- EPINET_train.py
+++ EPINET_train.py
@@ -105,7 +105,7 @@ if __name__ == '__main__':
                                gpu number = 0 ) 
     '''
     os.environ["CUDA_DEVICE_ORDER"]="PCI_BUS_ID"
-    os.environ["CUDA_VISIBLE_DEVICES"]="1"    
+    os.environ["CUDA_VISIBLE_DEVICES"]="0"    
 
     
     networkname='EPINET_train'
